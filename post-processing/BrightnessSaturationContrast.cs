b1994036dffac9e0623d53553b3df8d4b1daa8a1
Tong <56163550+FaithTong@users.noreply.github.com>
2019-11-22 00:00:05 +0800

    Update BrightnessSaturationContrast.cs

--- post-processing/BrightnessSaturationContrast.cs
+++ post-processing/BrightnessSaturationContrast.cs
@@ -6,59 +6,59 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class BrightnessSaturationContrast : MonoBehaviour
 {
-    //关联后期处理Shader
-    public Shader EffectShader;
+	//关联后期处理Shader
+	public Shader EffectShader;
 
-    //亮度、饱和度、对比度属性
-    public float Brightness = 1f;
-    public float Saturation = 1f;
-    public float Contrast = 1f;
+	//亮度、饱和度、对比度属性
+	public float Brightness = 1f;
+	public float Saturation = 1f;
+	public float Contrast = 1f;
 
-    //后期处理的材质
-    private Material EffectMaterial;
+	//后期处理的材质
+	private Material EffectMaterial;
 
-    //基于Shader生成的Material
-    Material CurrentMaterial
-    {
-        get
-        {
-            //判断Material是否为空，如果为空则创建Material
-            if (EffectMaterial == null)
-            {
-                EffectMaterial = new Material(EffectShader)
-                {
-                    //不会在hierarchy中显示，也不会保存到场景中
-                    hideFlags = HideFlags.HideAndDontSave
-                };
-            }
-            return EffectMaterial;
-        }
-    }
+	//基于Shader生成的Material
+	Material CurrentMaterial
+	{
+		get
+		{
+			//判断Material是否为空，如果为空则创建Material
+			if (EffectMaterial == null)
+			{
+				EffectMaterial = new Material(EffectShader)
+				{
+					//不会在hierarchy中显示，也不会保存到场景中
+					hideFlags = HideFlags.HideAndDontSave
+				};
+			}
+			return EffectMaterial;
+		}
+	}
 
-    //调用Shader进行后期处理
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        //判断有无关联Shader文件
-        //如果有，则进行属性传递
-        //如果没有，则不执行任何处理
-        if (EffectShader)
-        {
-            //将脚本中的属性传递给Shader
-            CurrentMaterial.SetFloat("_Brightness", Brightness);
-            CurrentMaterial.SetFloat("_Saturation", Saturation);
-            CurrentMaterial.SetFloat("_Contrast", Contrast);
+	//调用Shader进行后期处理
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		//判断有无关联Shader文件
+		//如果有，则进行属性传递
+		//如果没有，则不执行任何处理
+		if (EffectShader)
+		{
+			//将脚本中的属性传递给Shader
+			CurrentMaterial.SetFloat("_Brightness", Brightness);
+			CurrentMaterial.SetFloat("_Saturation", Saturation);
+			CurrentMaterial.SetFloat("_Contrast", Contrast);
 
-            Graphics.Blit(source, destination, EffectMaterial);
-        }
-        else
-            Graphics.Blit(source, destination);
-    }
+			Graphics.Blit(source, destination, EffectMaterial);
+		}
+		else
+			Graphics.Blit(source, destination);
+	}
 
-    //对开放的参数进行范围控制
-    private void Update()
-    {
-        Brightness = Mathf.Clamp(Brightness, 0f, 2f);
-        Saturation = Mathf.Clamp(Saturation, 0f, 2f);
-        Contrast = Mathf.Clamp(Contrast, 0f, 2f);
-    }
+	//对开放的参数进行范围控制
+	private void Update()
+	{
+		Brightness = Mathf.Clamp(Brightness, 0f, 2f);
+		Saturation = Mathf.Clamp(Saturation, 0f, 2f);
+		Contrast = Mathf.Clamp(Contrast, 0f, 2f);
+	}
 }
