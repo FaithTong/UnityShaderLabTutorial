ee94c75699ebfcf3c57111aaab0835b7ab249eb8
Tong <56163550+FaithTong@users.noreply.github.com>
2019-11-21 23:05:57 +0800

    post-processing

---
+++ post-processing/BrightnessSaturationContrast.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BrightnessSaturationContrast : MonoBehaviour
+{
+    #region
+    public Shader EffectShader;
+    public float Brightness = 1f;
+    public float Saturation = 1f;
+    public float Contrast = 1f;
+    private Material currentMaterial;
+    #endregion
+
+    Material EffectMaterial
+    {
+        get
+        {
+            if (currentMaterial == null)
+            {
+                currentMaterial = new Material(EffectShader)
+                {
+                    hideFlags = HideFlags.HideAndDontSave
+                };
+            }
+            return currentMaterial;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (EffectShader)
+        {
+            EffectMaterial.SetFloat("_Brightness", Brightness);
+            EffectMaterial.SetFloat("_Saturation", Saturation);
+            EffectMaterial.SetFloat("_Contrast", Contrast);
+
+            Graphics.Blit(source, destination, EffectMaterial);
+        }
+        else
+            Graphics.Blit(source, destination);
+    }
+
+    private void Update()
+    {
+        Brightness = Mathf.Clamp(Brightness, 0f, 2f);
+        Saturation = Mathf.Clamp(Saturation, 0f, 2f);
+        Contrast = Mathf.Clamp(Contrast, 0f, 2f);
+    }
+}
---
+++ post-processing/BrightnessSaturationContrast.shader
@@ -0,0 +1,49 @@
+﻿Shader "PostEffectShader/BrightnessSaturationContrast"
+{
+	Properties 
+	{
+		_MainTex("MainTex", 2D) = "white"{}
+		_Brightness("Brightness", float) = 1
+		_Saturation("Saturatioon", float) = 1
+		_Contrast("Contrast", float) = 1
+	}
+
+	Subshader 
+	{
+		Tags{"RenderType" = "Opaque"}
+		LOD 200
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#include "UnityCG.cginc"
+			#pragma target 3.0
+
+			sampler2D _MainTex;
+			half _Brightness;
+			half _Saturation;
+			half _Contrast;
+
+			fixed4 frag(v2f_img i) : SV_Target
+			{
+				fixed4 renderTex = tex2D(_MainTex, i.uv);
+
+				fixed3 finalColor = renderTex.rgb * _Brightness;
+			
+				fixed luminance = dot(finalColor, fixed3(0.2125, 0.7154, 0.0721));
+				fixed3 luminanceColor = fixed3(luminance, luminance, luminance);
+				finalColor = lerp(luminanceColor, finalColor, _Saturation);
+
+				fixed3 grayColor = fixed3(0.5, 0.5, 0.5);
+				finalColor = lerp(grayColor, finalColor, _Contrast);
+
+				return fixed4(finalColor, renderTex.a);
+			}
+			ENDCG
+		}
+	}
+	Fallback off
+}
