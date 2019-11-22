89669866ac11c4045fa59e538fd19d1d5fd94d27
Tong <56163550+FaithTong@users.noreply.github.com>
2019-11-22 09:11:30 +0800

    Update Properties.cs

--- post-processing/Properties.cs
+++ post-processing/Properties.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))] // 如果没有摄像机组件会自动创建
-[ExecuteInEditMode] // 在编辑状态也能执行
+[ExecuteInEditMode] // 在编辑状态也能执行脚本
 public class Properties : MonoBehaviour // 类名称要与脚本名称一致
 {
-	[Range(0.0f, 1.0f)] // 数值范围滑动条，只对下方第一个属性有效
-	public float myRange = 0;
+	[Range(0.0f, 1.0f)] // 将数值转变为滑动条，只对下方第一个属性有效
+	public float myRange = 0.5f;
 
-	public float myNumber = 0;
-	public bool myToggle = false;
-	public Color myColor = Color.white;
-	public Texture2D myTexture;
-	public Shader myShader;
-	public Material myMaterial;
-	public MeshFilter myMesh; // Mesh
+	public float myNumber = 0f; // 数值属性
+	public bool myToggle = false; // bool型开关属性
+	public Color myColor = Color.white; // 颜色属性
+	public Texture2D myTexture; // 关联纹理
+	public Shader myShader; // 关联Shader文件
+	public Material myMaterial; // 关联材质
+	public MeshFilter myMesh; // 关联场景中的模型
 }
