93e401d0a891a980107c70ddba885117f904bb79
Tong <56163550+FaithTong@users.noreply.github.com>
2019-11-29 13:44:19 +0800

    BSC

--- Post-processing/BSC.shader
+++ Post-processing/BSC.shader
@@ -5,13 +5,25 @@
         #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
 
         TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-        float _Blend;
+        half _Brightness;
+		half _Saturation;
+		half _Contrast;
 
         float4 Frag(VaryingsDefault i) : SV_Target
         {
             float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+
+			// 亮度
+			color.rgb *= _Brightness;
+
+			// 饱和度
             float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-            color.rgb = lerp(color.rgb, luminance.xxx, _Blend.xxx);
+            color.rgb = lerp(luminance, color.rgb, _Saturation);
+
+			// 对比度
+			half3 grayColor = half3(0.5, 0.5, 0.5);
+			color.rgb = lerp(grayColor, color.rgb, _Contrast);
+
             return color;
         }
 
--- Post-processing/GSC.cs
+++ Post-processing/GSC.cs
@@ -8,8 +8,15 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/BSC")]
 public sealed class BSC : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
-    public FloatParameter blend = new FloatParameter { value = 0.5f };
+    //开放属性
+    [Range(0f, 2f), Tooltip("Brightness effect intensity.")]
+    public FloatParameter Brightness = new FloatParameter { value = 1f };
+
+    [Range(0f, 2f), Tooltip("Saturation effect intensity.")]
+    public FloatParameter Saturation = new FloatParameter { value = 1f };
+
+    [Range(0f, 2f), Tooltip("Contrast effect intensity.")]
+    public FloatParameter Contrast = new FloatParameter { value = 1f };
 }
 
 public sealed class GrayscaleRenderer : PostProcessEffectRenderer<BSC>
@@ -17,7 +24,12 @@ public sealed class GrayscaleRenderer : PostProcessEffectRenderer<BSC>
     public override void Render(PostProcessRenderContext context)
     {
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/BSC-HLSL"));
-        sheet.properties.SetFloat("_Blend", settings.blend);
+
+        //传递属性
+        sheet.properties.SetFloat("_Brightness", settings.Brightness);
+        sheet.properties.SetFloat("_Saturation", settings.Saturation);
+        sheet.properties.SetFloat("_Contrast", settings.Contrast);
+
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
